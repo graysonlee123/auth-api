1cef6e7217d0bcb4f79924b3d0318b5f65bf8064
graysonlee123 <graysonleegantek@gmail.com>
2019-10-13 00:21:24 -0400

    adds validation using joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,28 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+// Validation
+const Joi = require('@hapi/joi');
+
+const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+}
+
 router.post('/register', async (req,res) => {
+    // Validate request data
     const {name, email, password} = req.body;
+    const {error} = Joi.validate(
+        {
+            name: name,
+            email: email,
+            password: password
+        }, 
+        schema
+    );
+    
+    if (error) return res.status(400).send(error.details[0].message);
 
     const user = new User({
         name: name,
