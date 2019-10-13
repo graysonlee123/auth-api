3bdbaa1d73a06231b9780ecd7c35b7ec20781882
graysonlee123 <graysonleegantek@gmail.com>
2019-10-13 00:40:19 -0400

    separates authentication logic into it's own file

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
-
-// Validation
-const Joi = require('@hapi/joi');
-
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-}
+const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req,res) => {
-    // Validate request data
-    const {name, email, password} = req.body;
-    const {error} = Joi.validate(
-        {
-            name: name,
-            email: email,
-            password: password
-        }, 
-        schema
-    );
-    
+    // Validate request data   
+    const {error} = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const user = new User({
-        name: name,
-        email: email,
-        password: password
-    });
+    // Disallow duplicate emails
+    const emailExists = await User.findOne({email: req.body.email});
+    if (emailExists) return res.status(400).send('Email already exists');
 
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    
     try {
         const savedUser = await user.save();
         res.send(savedUser);
