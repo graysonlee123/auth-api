5fc66813111c89d84853d758d7f01824e83c80bf
graysonlee123 <graysonleegantek@gmail.com>
2019-10-12 23:38:51 -0400

    sets up database connection

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const port = process.env.PORT || 8000;
 
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => {
+        console.log('Connected to database!');
+    }
+);
+
+// Import routes
 const authRoute = require('./routes/auth');
 
+// Routes middleware
 app.use('/api/user', authRoute);
 
 app.listen(port, () => {
