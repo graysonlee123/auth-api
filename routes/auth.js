07ce1b6f34b4453f3feae4dfa3198ffcd7763056
graysonlee123 <graysonleegantek@gmail.com>
2019-10-13 00:49:44 -0400

    adds password salting/hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req,res) => {
@@ -11,10 +12,14 @@ router.post('/register', async (req,res) => {
     const emailExists = await User.findOne({email: req.body.email});
     if (emailExists) return res.status(400).send('Email already exists');
 
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     
     try {
